7b5c699583930f8a348dfde5c5e56349e4ead90e
Anuska <ank.sampedro@gmail.com>
2019-08-06 14:30:09 +0200

    redux done example

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 import SongList from '../Components/SongList/SongList';
+import SongDetail from '../Components/SongDetail/SongDetail';
 
 const App = () => {
-  return (
-    <div className="ui container grid">
-      <div className="ui row"><div className="column eight wide"><SongList /></div></div>
-    </div>
-  );
-}
+	return (
+		<div className="ui container grid">
+			<div className="ui row">
+				<div className="column eight wide">
+					<SongList />
+				</div>
+				<div className="column eight wide">
+					<SongDetail />
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
